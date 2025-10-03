e6d7f4227cdb831d4b885b0a2dfecfc17e921951
Elbarae1921 <elbarae1921@gmail.com>
2025-10-03 22:58:57 +0100

    feat: setup CI

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -189,3 +189,45 @@ resource "aws_iam_role_policy_attachment" "microtodo_ebs_csi_driver_policy" {
   role       = aws_iam_role.microtodo_ebs_csi_driver.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
+
+# IAM Identity Provider for GitHub Actions
+data "tls_certificate" "gha" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.gha.certificates[0].sha1_fingerprint]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "github_actions_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github_actions.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(aws_iam_openid_connect_provider.github_actions.url, "https://", "")}:aud" = "sts.amazonaws.com"
+            "${replace(aws_iam_openid_connect_provider.github_actions.url, "https://", "")}:sub" = "repo:micro-todo/*:ref:refs/heads/master"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_policy" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions.arn
+}
